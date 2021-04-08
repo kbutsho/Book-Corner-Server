45ced583efc68a6b8fd1edfebf885ce7d3eebfa4
Kbutsho <kbutsho@yahoo.com>
2021-04-08 19:20:50 +0600

    update

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 const ObjectId = require('mongodb').ObjectId;
 
-
 const app = express()
 app.use(cors());
 app.use(express.json());
@@ -16,44 +15,57 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 client.connect(err => {
   const BooksCollection = client.db("BookCorner").collection("BooksCollection");
- 
+  const OrderCollection = client.db("BookCorner").collection("orderCollection");
+
   app.post('/addBook', (req, res) => {
     const newBooks = req.body;
     BooksCollection.insertOne(newBooks)
-        .then(result => {
-            console.log('inserted count', result.insertedCount);
-            res.send(result.insertedCount > 0)
-        })
-})
-app.get('/books', (req,res)=>{
-  BooksCollection.find()
-  .toArray((err,items)=>{
-      res.send(items);
-      console.log(" Get data from Database", items);
-  })
-})
-
-// read to UI
-app.get('/getBooks', (req, res) => {
-  BooksCollection.find({})
-      .toArray((err, documents) => {
-          res.send(documents);
-      })
-})
-app.delete('/delete/:id', (req, res) => {
-  BooksCollection.findOneAndDelete({ _id: ObjectId(req.params.id) })
       .then(result => {
-         res.send(result.deletedCount > 0);
+        res.send(result.insertedCount > 0)
       })
-})
+  })
+  app.get('/books', (req, res) => {
+    BooksCollection.find()
+      .toArray((err, items) => {
+        res.send(items);
+      })
+  })
 
-
-
-
+  app.get('/getBooks', (req, res) => {
+    BooksCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+  app.delete('/delete/:id', (req, res) => {
+    BooksCollection.findOneAndDelete({ _id: ObjectId(req.params.id) })
+      .then(result => {
+        res.send(result.deletedCount > 0);
+      })
+  })
+  app.get('/book/:id', (req, res) => {
+    BooksCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+  // book order
+  app.post('/addBooking', (req, res) => {
+    const newBooking = req.body;
+    OrderCollection.insertOne(newBooking)
+      .then(result => {
+        res.send(result.insertedCount > 0);
+      })
+  })
+  // get bookings data
+  app.get('/bookings', (req, res) => {
+    OrderCollection.find({email: req.query.email})
+      .toArray((err, documents) => {
+        res.send(documents)
+      })
+  })
   console.log("Database Connected");
 });
-
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
